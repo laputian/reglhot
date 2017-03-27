834838f87d328afde1d0ea7d2a91756da6e04c8d
tito <tttito@gmail.com>
2017-03-27 14:17:41 +0200

    Lob first, withot elasticiy

--- lob/simple_lob.py
+++ lob/simple_lob.py
@@ -1,15 +1,17 @@
 import numpy as np
 
-lob_dim = 32
+lob_dim = 80
 start_book_depth = 110.
 start_midPrice = 100.
-tick_size = 1.
+tick_size = 2.
 lob_tick_depth = 0 * np.ones(lob_dim)
 lob = start_book_depth + (lob_tick_depth * np.ones(lob_dim))
 
 ord_range = (lob_dim - 1)//2
 
-order = ['S', 105]
+ord_load = 61
+
+order = ['S', ord_load]
 
 buy_range = range(ord_range + 1, lob_dim)
 sell_range = range(ord_range, 0, -1)
@@ -18,11 +20,17 @@ def extrinRange(lob, the_range):
     for k in the_range:
         if lob[k] > 0:
             return k
+    return -1
 
 def midPrice(lob, buy_range, sell_range):
-    buy_min = start_midPrice + tick_size * extrinRange(lob, buy_range)
-    sell_max = start_midPrice + tick_size * extrinRange(lob, sell_range)
-    return (buy_min + sell_max)/2
+    buy_min = start_midPrice + tick_size * (extrinRange(lob, buy_range) - ord_range)
+    print('extr in buy range',extrinRange(lob, buy_range))
+    print('buy_min', buy_min)
+    sell_max = start_midPrice - tick_size * (ord_range - extrinRange(lob, sell_range))
+    print('extr in sell range', extrinRange(lob, sell_range))
+    print('sell_max', sell_max)
+    return (buy_min + sell_max)/2.
+
 
 def lob_order(lob, the_range, ord_residual):
     for k in the_range:
@@ -32,43 +40,45 @@ def lob_order(lob, the_range, ord_residual):
         if ord_hold <= 0. :
             break
         ord_residual = ord_hold
+    print(lob)
     return lob
-
-print(midPrice(lob, buy_range, sell_range))
 
 if order[0] == 'B':
     the_range = buy_range
 else:
     the_range = sell_range
 
-ord_residual = order[1]
+
+def market_run(lob = lob, test_range = 50, buy_range = buy_range, sell_range = sell_range, ord_load = ord_load)  :
+    mids = []
+    for i in range(test_range):
+        if (i <test_range/3 or i > 3*test_range/4):
+            the_range = sell_range
+        else:
+            the_range = buy_range
+        lob = lob_order(lob, the_range, ord_load)
+        mid = midPrice(lob, buy_range, sell_range)
+        print('mid', mid)
+        mids.append(mid)
+    return mids
 
 
-lob = lob_order(lob, the_range, ord_residual)
-print(lob)
-print(midPrice(lob, buy_range, sell_range))
-
-mids =   []
-
-test_range = 50
-
-for i in range(test_range):
-    order = 50
-    if (i <test_range/3 or i > 3*test_range/4):
-        the_range = sell_range
-    else:
-        the_range = buy_range
-    lob_order(lob, the_range, order)
-    print(lob)
-    mid = midPrice(lob, the_range, sell_range)
-    print(mid)
-    mids.append(mid)
 
 import matplotlib.pyplot as plt
-plt.plot(mids)
-plt.ylabel('stock price')
-plt.ylabel('time')
-plt.show()
+def show_mids(mids):
+    plt.plot(mids)
+    plt.ylabel('stock price')
+    plt.xlabel('time')
+    plt.title("Limit order book trading")
+    plt.ylim(80,120)
+    plt.show()
+
+mids = market_run(lob,  100, buy_range , sell_range)
+show_mids(mids = mids)
+
+
+
+
 
 
 
