43f58effa3d47b646776ee36f03189b2a7282742
tito <tttito@gmail.com>
2017-04-03 09:09:14 +0200

    Plotting midprice

--- lob/simple_lob_adapt.py
+++ lob/simple_lob_adapt.py
@@ -3,17 +3,17 @@ import numpy as np
 lob_dim = 10
 start_book_depth = 110.
 start_midPrice = 100.
-tick_size = 5.
+tick_size = 1.
 lob = start_book_depth + np.zeros(lob_dim)
-num_step =10
+num_step =50
 
 print_diagnostics = True
 
 ord_range = (lob_dim - 1)//2
 
-ord_load = 135
+ord_load = 35
 
-order = ['S', ord_load]
+order = ['B', ord_load]
 
 if print_diagnostics:
     print(lob)
@@ -27,17 +27,6 @@ def extrinRange(lob, the_range):
         if lob[k] > 0:
             return k
     return -1
-
-def midPrice(lob, buy_range, sell_range, ord_range = ord_range):
-    buy_min = start_midPrice + tick_size * (extrinRange(lob, buy_range) - ord_range)
-    sell_max = start_midPrice - tick_size * (ord_range - extrinRange(lob, sell_range))
-    if print_diagnostics:
-        print(order[0])
-        print('extr in sell range', extrinRange(lob, sell_range))
-        print('sell_max', sell_max)
-        print('extr in buy range',extrinRange(lob, buy_range))
-        print('buy_min', buy_min)
-    return (buy_min + sell_max)/2.
 
 def resetLob(lob, buy_range, sell_range):
     buy_index = extrinRange(lob, buy_range)
@@ -59,51 +48,56 @@ def lob_order(lob, the_range, ord_residual, ord_range = ord_range):
     if step > 0:
         for h in range(step if step > 1 else 0):
             lob[ord_range - h] = lob_c[ord_range - step -h + 1]
+        step = - step + 1
     else:
         for h in range(-step + 1):
             lob[ord_range + h +1] = lob_c[ord_range - step + h + 1]
+        step = -step
     if print_diagnostics:
+        print(order[0])
+        print('step = ', step)
         print(lob)
         print('extreme for this order', extrinRange(lob, the_range))
-    return lob
+    return lob, step
 
 
 
-def market_run(lob = lob, test_range = num_step, buy_range = buy_range, sell_range = sell_range, order = order)  :
+def market_run(lob = lob, test_range = num_step, buy_range = buy_range, sell_range = sell_range, order = order, price = start_midPrice)  :
     mids = []
+    steptot = 0
     for i in range(test_range):
         if (order[0] =='S'):
             the_range = sell_range
         else:
             the_range = buy_range
-        lob = lob_order(lob, the_range, order[1])
-        mid = midPrice(lob, buy_range, sell_range)
-        if print_diagnostics:
-            print('mid', mid)
-        mids.append(mid)
-    return lob, mids
+        lob, step = lob_order(lob, the_range, order[1])
+        steptot = steptot + step
+        priceTot = price  + steptot * tick_size
+        mids.append(priceTot)
+    return lob, mids, priceTot
 
 
 
 
 
 import matplotlib.pyplot as plt
-def show_mids(mids):
+def show_mids(mids, max, min):
     plt.plot(mids)
     plt.ylabel('stock price')
     plt.xlabel('time')
     plt.title("Limit order book trading")
-    plt.ylim(85,110)
+    plt.ylim( max +2, min -2)
     plt.show()
 
 if __name__ == "__main__":
-    lob, mids = market_run(lob, num_step, buy_range, sell_range, order)
+    lob, mids, priceTot = market_run(lob, num_step, buy_range, sell_range, order)
     if order[0] == 'B':
         order[0]  = 'S'
     else:
         order[0] = 'B'
-    mids = mids + market_run(lob, num_step, buy_range, sell_range, order)[1]
-    show_mids(mids=mids)
+    mids = [start_midPrice] + mids + market_run(lob, num_step, buy_range, sell_range, order, price = priceTot)[1]
+    max, min = np.amax(mids),  np.amin(mids)
+    show_mids(mids=mids, max = max, min = min)
 
 
 
