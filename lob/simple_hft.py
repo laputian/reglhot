cfb8fa5c9bec7dc22c25e5160a594398fa8c2551
tito <tttito@gmail.com>
2017-04-07 11:42:41 +0200

    Plotting composite_poisson market orders

--- lob/simple_hft.py
+++ lob/simple_hft.py
@@ -47,13 +47,19 @@ def distr_mos(size = 100., alpha= 1.5):
         return 1.
 
 import numpy as np
-def test_poisson():
+def test_poisson_1():
     tot = 0
     for k in range(10):
         s = np.random.poisson(1)
         tot = tot + s
         print(s)
     print(tot)
+
+def test_poisson_2():
+    s = np.random.poisson(1, 10)
+    print(s)
+    print(s.sum())
+
 
 import matplotlib.pyplot as plt
 def plot_size():
@@ -69,10 +75,7 @@ def plot_size():
 import scipy.stats as st
 from scipy.integrate import quad
 
-
-
-
-class my_pdf(st.rv_continuous):
+class market_order_pdf(st.rv_continuous):
     gabaix_exp = 1.53
     def gabaix_func(self, x):
         if x >= 1:
@@ -83,24 +86,73 @@ class my_pdf(st.rv_continuous):
     def _pdf(self,x):
         return self.gabaix_func(x)/self.gabaix_exp #/self.measure_integral()[0]# Normalized over its range, in this case [0,1]
 
+
 def f(x):
     return 1
 
-def test_distrib():
-    my_cv = my_pdf(a=1, b=100, name='my_pdf')
-    print(my_cv.expect(f))
-    print(my_cv.cdf(1.1))
-    a = my_cv.rvs(size=100)
-    b = np.zeros((100))
-    for k in range(100):
-        b[k] = 100*my_cv.gabaix_func(k+2)
+def test_distrib_1():
+    the_upper_bound = math.inf
+    the_range = 100
+    the_base_order_size = 100
+    mko_cv = market_order_pdf(a=1, b=the_upper_bound, name='mko_pdf')
+    # print(mko_cv.expect(f))
+    # print(mko_cv.cdf(1.1))
+    a = mko_cv.rvs(size=the_range)
+    b = np.zeros((the_range))
+    for k in range(the_range):
+        b[k] = the_base_order_size * mko_cv.gabaix_func(k + 2)
     print(a)
+    plt.title("Order Sizes and density function for x > 1.  Mean = " + str(round(a.sum()/the_range, 2)))
+    plt.xlabel("Iterator")
+    plt.ylabel("Order site and density value")
     plt.plot(b)
     plt.plot(a)
     plt.show()
 
+import seaborn as sns
+
+def test_mko_pdf():
+    the_upper_bound = math.inf
+    mko_cv = market_order_pdf(a=1, b=the_upper_bound, name='mko_pdf')
+    a = mko_cv.rvs(size=100)
+    sns.distplot(a, bins=10, kde=False, rug=True)
+    plt.show()
+
+def test_distrib_with_poisson():
+    plot_hist = True
+    the_upper_bound = math.inf
+    the_range = 1000
+    the_base_order_size = 100
+    s = np.random.poisson(1, the_range)
+    s_tot = s.sum()
+    mko_cv = market_order_pdf(a=1, b=the_upper_bound, name='mko_pdf')
+    a = mko_cv.rvs(size=s_tot)
+    tot_a = np.zeros((the_range))
+    b = np.zeros((the_range))
+    for k in range(the_range):
+        a_index = 0
+        k_tot = 0.
+        for j in range(s[k]):
+            k_tot = a[a_index + 1] + k_tot
+            a_index = a_index + 1
+        tot_a[k] = k_tot
+    print(tot_a)
+    if plot_hist:
+        sns.distplot(tot_a, bins=10, kde=False, rug=True)
+    else:
+        plt.title("Order Sizes and and number of orders.  Mean = " + str(round(tot_a.sum() / s_tot, 2)))
+        plt.xlabel("Iterator")
+        plt.ylabel("Total order site and number of orders")
+        plt.plot(s)
+        plt.plot(tot_a)
+    plt.show()
+
 if __name__ == "__main__":
-    test_distrib()
+    #test_distrib_with_poisson()
+    #test_distrib_1()
+    test_mko_pdf()
+
+
 
 
 
